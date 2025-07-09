68f22df224aab6aa9745b9ba93b909085ec5caa7
Rohit <rohit_ec22a18_07@dtu.ac.in>
2025-07-09 10:24:35 +0530

    Serve frontend from backend and update .gitignore

--- Backend/app.js
+++ Backend/app.js
@@ -11,7 +11,7 @@ const mapsRoutes = require('./routes/maps.routes')
 const rideRoutes = require('./routes/ride.routes')
 
 connectToDb();
-
+app.use(express.static(path.join(__dirname, '../Frontend/dist')));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -27,5 +27,8 @@ app.use('/users', userRoutes);
 app.use('/captains', captainRoutes); 
 app.use('/maps', mapsRoutes)
 app.use('/rides', rideRoutes)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../Frontend/dist/index.html'));
+});
 
 module.exports = app;
